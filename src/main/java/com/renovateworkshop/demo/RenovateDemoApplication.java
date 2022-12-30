6162b4a747fee251184af4ed68586ede63c82ef3
MarienL1995 <70948300+MarienL1995@users.noreply.github.com>
2022-12-30 15:34:58 +0100

    boilerplate setup

---
+++ src/main/java/com/renovateworkshop/demo/RenovateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.renovateworkshop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renovateworkshop/demo/RenovateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renovateworkshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
